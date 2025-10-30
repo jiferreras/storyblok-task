48fba93cc3d16c9a5263a408c7cffd352a1cc740
Juan <juanignaci3@gmail.com>
2025-10-30 11:48:05 +0000

    maybe fix?

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { StoryblokComponent, useStoryblok } from '@storyblok/react';
+import { StoryblokComponent, useStoryblok, useStoryblokBridge } from '@storyblok/react';
 import { useParams } from 'react-router';
 
 export default function App() {
     const currentYear = new Date().getFullYear();
     const { '*': slug } = useParams();
     const story = useStoryblok(slug || 'home', {
-        version: import.meta.env.STORYBLOK_IS_PREVIEW === 'true' ? 'draft' : 'published',
+        version: 'draft'
     });
     if (!story?.content) {
         return <div>Loading...</div>;
     }
+
     return (
         <>
             <StoryblokComponent blok={story.content} />

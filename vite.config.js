bf40fc4fa33d32cb8e12476580b93996fdf0214d
Juan <juanignaci3@gmail.com>
2025-10-30 23:12:42 +0000

    fixed things

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
 import process from 'process'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,6 +10,12 @@ export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	return {
 		plugins: [react(), mkcert()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
         envDir: '.',
 		define: {
 			'import.meta.env.STORYBLOK_DELIVERY_API_TOKEN': JSON.stringify(

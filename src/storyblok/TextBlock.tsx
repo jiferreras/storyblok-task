69d7949474ff124f91c498f9713f79b1ea49b7cf
Juan <juanignaci3@gmail.com>
2025-10-31 12:44:12 +0000

    fixed margins

--- src/storyblok/TextBlock.tsx
+++ src/storyblok/TextBlock.tsx
@@ -8,7 +8,7 @@ export default function TextBlock({ blok }: BlokData<Components.TextBlock>) {
 
     const content = blok.content.content.map((item) => (
         item.content?.map((item, index) => (
-            <p key={index} {...item.attrs} className='mb-4 max-w-none mx-auto text-justify p-4'>{item.text}</p>
+            <p key={index} {...item.attrs} className='mb-4 max-w-none mx-auto text-justify p-4 -mt-6'>{item.text}</p>
         )) || null
     ));
 

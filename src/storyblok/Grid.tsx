3fb7388dbdb8c86757e717c93aa7e73683b086ac
Juan <juanignaci3@gmail.com>
2025-10-31 12:13:08 +0000

    grid fix

--- src/storyblok/Grid.tsx
+++ src/storyblok/Grid.tsx
@@ -3,9 +3,11 @@ import { BlokData, Components } from './utils';
 
 export default function Grid({ blok }: BlokData<Components.Grid>) {
     return (
-        <div {...storyblokEditable(blok)} className="grid">
+        <div {...storyblokEditable(blok)} className="grid flex flex-col md:flex-row">
             {blok.columns?.map((nestedBlok) => (
-                <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+                <div className="w-full md:w-1/2" key={nestedBlok._uid}>
+                    <StoryblokComponent blok={nestedBlok} />
+                </div>
             ))}
         </div>
     );

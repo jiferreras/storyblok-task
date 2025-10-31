2df634656836f2f61859e8e79d8812fa77ccc5d0
Juan <juanignaci3@gmail.com>
2025-10-31 12:16:11 +0000

    fix

--- src/storyblok/Grid.tsx
+++ src/storyblok/Grid.tsx
@@ -3,7 +3,7 @@ import { BlokData, Components } from './utils';
 
 export default function Grid({ blok }: BlokData<Components.Grid>) {
     return (
-        <div {...storyblokEditable(blok)} className="grid flex flex-row flex-wrap">
+        <div {...storyblokEditable(blok)} className="flex flex-row flex-wrap">
             {blok.columns?.map((nestedBlok) => (
                 <div className="w-full md:w-1/2 mb-4" key={nestedBlok._uid}>
                     <StoryblokComponent blok={nestedBlok} />
